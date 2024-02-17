fbec029e0f50f2e38ab5d57ff22c4839abe6b156
INVOKER <developer.invoker@gmail.com>
2024-02-17 13:45:32 +0330

    create filter section

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,3 +1,19 @@
 export const shortenText = (text) => {
   return text.split(" ").slice(0, 3).join("");
 };
+
+export const searchProducts = (products, search) => {
+  if (!search) return products;
+  const searchedProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(search)
+  );
+  return searchedProducts;
+};
+
+export const filterProducts = (products, category) => {
+  if (!category) return products;
+  const filteredProducts = products.filter(
+    (product) => product.category === category
+  );
+  return filteredProducts;
+};
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 function NotFoundPage() {
-  return <div className="background h-screen">NotFoundPage</div>;
+  return <div className="   ">NotFoundPage</div>;
 }
 
 export default NotFoundPage;
