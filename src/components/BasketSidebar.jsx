0f19432a274af0936986e90d547155d31251dd91
INVOKER <developer.invoker@gmail.com>
2024-02-20 18:46:11 +0330

    add a checkout btn

--- src/components/BasketSidebar.jsx
+++ src/components/BasketSidebar.jsx
@@ -2,9 +2,9 @@ import { MdPriceChange } from "react-icons/md";
 import { FaHashtag } from "react-icons/fa";
 import { BsFillPatchCheckFill } from "react-icons/bs";
 
-function BasketSidebar({ state }) {
+function BasketSidebar({ state, clickHandler }) {
   return (
-    <div className="w-full md:w-1/3 rounded-xl border-2 mt-6 md:py-4">
+    <div className="w-full md:w-1/3 rounded-xl border-2 mt-6 py-4 ">
       <div className="w-full px-4 py-2 space-y-2 md:space-y-5 font-bold text-white md:text-lg">
         <div className="w-full flex justify-start items-center">
           <div className="flex justify-center items-center gap-x-1">
@@ -13,7 +13,9 @@ function BasketSidebar({ state }) {
             </span>
             <span>Total:</span>
           </div>
-          <div className="text-sm md:text-lg font-semibold  pl-2 text-green-500">{state.total} $</div>
+          <div className="text-sm md:text-lg font-semibold  pl-2 text-green-500">
+            {state.total} $
+          </div>
         </div>
         <div className="w-full flex justify-start items-center">
           <div className="flex justify-center items-center gap-x-1">
@@ -22,7 +24,9 @@ function BasketSidebar({ state }) {
             </span>
             <span>Quantity:</span>
           </div>
-          <div className="text-sm md:text-lg font-semibold text-orange-500 pl-2 ">{state.itemsCounter}</div>
+          <div className="text-sm md:text-lg font-semibold text-orange-500 pl-2 ">
+            {state.itemsCounter}
+          </div>
         </div>
         <div className="w-full flex justify-start items-center">
           <div className="flex justify-center items-center gap-x-1">
@@ -31,9 +35,16 @@ function BasketSidebar({ state }) {
             </span>
             <span>Status:</span>
           </div>
-          <div className="text-sm md:text-lg font-semibold text-yellow-500 pl-2 ">{!state.checkout && "Pending..."}</div>
+          <div className="text-sm md:text-lg font-semibold text-yellow-500 pl-2 ">
+            {!state.checkout && "Pending..."}
+          </div>
         </div>
       </div>
+      <button onClick={() => clickHandler("CHECKOUT")} className="w-full px-5 mt-4">
+        <div className="w-full bg-white rounded-xl py-1 font-bold text-blue-600 text-lg">
+          Checkout
+        </div>
+      </button>
     </div>
   );
 }
