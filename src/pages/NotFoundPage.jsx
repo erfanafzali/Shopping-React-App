0cf8dde8316469c0fd56d59db7c62a35b050c562
INVOKER <developer.invoker@gmail.com>
2024-02-20 18:27:41 +0330

    end project

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 function NotFoundPage() {
-  return <div className="">NotFoundPage</div>;
+  return <div className="w-full text-center text-yellow-500 font-bold text-3xl pt-32 background h-screen">NotFoundPage🙄</div>;
 }
 
 export default NotFoundPage;
